3841b69356bf02ef1dc7fef07215c757d2fe8b20
warleyhenrique <warley.henrique93@gmail.com>
2021-09-03 13:37:06 -0300

    finaliy test for the capterII

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,18 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error("user not found");
+    }
+    if (!user.admin) {
+      throw new Error("this user not permission");
+    }
+
+    const users = this.usersRepository.list();
+
+    return users;
   }
 }
 
